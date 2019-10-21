9193fb82043fbae0df76c792f279c7edef40d32b
Simon Kalt <ska@kialo.com>
2019-10-21 13:21:19 +0200

    Formatting

--- examples/analyze_stub_analyzer.py
+++ examples/analyze_stub_analyzer.py
@@ -2,14 +2,16 @@ import os
 from pathlib import Path
 
 # goes through all python stubs (.pyi files) and lists their exported symbols with type annotation
-from stub_analyzer.api import get_stub_types, _print_graph_data
+from stub_analyzer.api import _print_graph_data, get_stub_types
 
-base_dir = Path(os.path.dirname(os.path.abspath(__file__))) / '..'
+base_dir = Path(os.path.dirname(os.path.abspath(__file__))) / ".."
 mypy_conf = base_dir / "mypy.ini"
-hand_written = get_stub_types(
-    f"{base_dir}/stubs-handwritten", mypy_conf_path=str(mypy_conf)
+hand_written = list(
+    get_stub_types(f"{base_dir}/stubs-handwritten", mypy_conf_path=str(mypy_conf))
 )
-generated = get_stub_types(f"{base_dir}/stubs-generated", mypy_conf_path=str(mypy_conf))
+generated = list(
+    get_stub_types(f"{base_dir}/stubs-generated", mypy_conf_path=str(mypy_conf))
+)
 
 print("Hand-written Stubs:\n")
 _print_graph_data(hand_written)
