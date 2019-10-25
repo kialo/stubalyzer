9229852d02c77161a3070f358711463a9cda00bb
Mathias Kahl <mk@kialo.com>
2019-10-25 14:57:07 +0200

    added __main__.py to allow executing stub-analyzer as module

---
+++ stub_analyzer/__main__.py
@@ -0,0 +1,9 @@
+from stub_analyzer.analyze import main
+
+"""
+This file allows stub-analyzer to be executed as a module like this:
+python -m stub_analyzer --config mypy.ini /path/to/stubs
+"""
+
+if __name__ == "__main__":
+    main()
