04f6404137669a2b119d2228bfe2cf0cf493e19c
Simon Kalt <ska@kialo.com>
2019-10-25 16:16:16 +0200

    Mock modules for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -55,6 +55,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 html_theme = "sphinx_rtd_theme"
 
+autodoc_mock_imports = ["mypy", "schema"]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
