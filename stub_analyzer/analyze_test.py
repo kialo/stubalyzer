b62010c3764f87e2cadeef3c8ce33d2ee8eb5959
Mathias Kahl <mk@kialo.com>
2019-10-28 17:43:57 +0100

    simple test of the command line tool

--- stub_analyzer/analyze_test.py
+++ stub_analyzer/analyze_test.py
@@ -1,8 +1,13 @@
+import re
 from typing import Any
+from unittest.mock import patch
+
+import pytest
+from _pytest.capture import CaptureFixture
 
 from testing.util import MypyNodeFactory, WithStubTestConfig
 
-from .analyze import analyze_stubs, compare
+from .analyze import analyze_stubs, compare, main
 from .compare import MatchResult
 
 
@@ -21,12 +26,12 @@ class TestAnalyzeStubs(WithStubTestConfig):
 
         assert "missing.MISSING_CONSTANT" not in err
         assert (
-            'Expected "missing.missing_function" to be "mismatch" but it was "not_found"'
-            in err
+            'Expected "missing.missing_function" to be "mismatch" but it was '
+            '"not_found" ' in err
         )
         assert (
-            'Expected "missing.missing_decorator" to fail, but it was not even processed'
-            in err
+            'Expected "missing.missing_decorator" to fail, but it was not even '
+            "processed " in err
         )
         assert 'Symbol "missing.MissingClass" not found in generated stubs' in err
 
@@ -42,8 +47,8 @@ class TestAnalyzeStubs(WithStubTestConfig):
 
         assert "mismatching.mismatching_function" not in err
         assert (
-            'Expected "mismatching.MISMATCHING_CONSTANT" to be "not_found" but it was "mismatch"'
-            in err
+            'Expected "mismatching.MISMATCHING_CONSTANT" to be "not_found" but it was '
+            '"mismatch" ' in err
         )
         assert "Types for mismatching.mismatch_variable do not match" in err
 
@@ -80,3 +85,49 @@ class TestCompareSymbols:
         result = list(compare([class_symbol], []))
 
         assert map(lambda x: x.match_result is MatchResult.NOT_FOUND, result)
+
+
+class TestCommandLineTool:
+    @patch("sys.argv", ["analyze-stubs"])
+    def test_missing_required_arguments(self, capsys: CaptureFixture) -> None:
+        with pytest.raises(SystemExit) as ex:
+            main()
+        assert ex.value.code == 2
+
+        output = capsys.readouterr().err
+
+        assert re.search(r"analyze-stubs: error", output)
+        assert re.search(
+            r"the following arguments are required: -c/--config, stubs_handwritten",
+            output,
+        )
+
+    @patch(
+        "sys.argv",
+        [
+            "analyze-stubs",
+            "-c",
+            "mypy.ini",
+            "testing/test-stubs/test_generated_reference_stubs",
+        ],
+    )
+    def test_generated_reference_stubs(self, capsys: CaptureFixture) -> None:
+        """
+        Ensures that analyzing stubs with automatically generated reference
+        stubs works as expected.
+        """
+        with pytest.raises(SystemExit) as ex:
+            main()
+        assert ex.value.code == 1
+
+        output = capsys.readouterr().err
+
+        # incorrectly stubbed
+        assert re.search(r"Types for black.DEFAULT_LINE_LENGTH do not match", output)
+        assert re.search(r"Types for black.shutdown do not match", output)
+        assert re.search(
+            r"Symbol \"black.NotFound\" not found in generated stubs", output
+        )
+
+        # cancel has been correctly stubbed
+        assert not re.search(r"cancel", output)
---
+++ testing/test-stubs/test_generated_reference_stubs/black.pyi
@@ -0,0 +1,16 @@
+import asyncio
+from typing import Iterable
+
+DEFAULT_LINE_LENGTH: str
+
+
+def shutdown(loop: str) -> None:
+    ...
+
+
+class NotFound:
+    pass
+
+
+def cancel(tasks: Iterable[asyncio.Task]) -> None:
+    ...
