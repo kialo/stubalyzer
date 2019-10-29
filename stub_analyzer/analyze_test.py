9ff2dc52502d31c5b424a77f82464fe0a54f9e5a
Zemre Arslan <za@kialo.com>
2019-10-29 09:52:07 +0100

    Fix analyze tests for comparing

--- stub_analyzer/analyze_test.py
+++ stub_analyzer/analyze_test.py
@@ -78,13 +78,13 @@ class TestCompareSymbols:
         func_def_symbol, _ = mypy_nodes.get_missing_function_node()
         result = list(compare([func_def_symbol], []))
 
-        assert map(lambda x: x.match_result is MatchResult.NOT_FOUND, result)
+        assert all(map(lambda x: x.match_result is MatchResult.NOT_FOUND, result))
 
     def test_generated_is_missing_a_class(self, mypy_nodes: MypyNodeFactory) -> None:
         class_symbol, _ = mypy_nodes.get_missing_class()
         result = list(compare([class_symbol], []))
 
-        assert map(lambda x: x.match_result is MatchResult.NOT_FOUND, result)
+        assert all(map(lambda x: x.match_result is MatchResult.NOT_FOUND, result))
 
 
 class TestCommandLineTool:
