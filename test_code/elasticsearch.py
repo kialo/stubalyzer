95e2888ab2ad1eef4c57c99b825b45079da5bfed
Simon Kalt <ska@kialo.com>
2019-10-29 11:44:22 +0100

    Remove test code

--- test_code/elasticsearch.py
+++
@@ -1,6 +0,0 @@
-from elasticsearch.client.indices import IndicesClient
-
-
-def create() -> None:
-    client = IndicesClient(None)
-    client.create(index=None, body=None, params=None, include_type_name=True)
--- test_code/run_mypy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname $0)/.."
-
-echo "Running mypy with generated stubs"
-MYPYPATH=./stubs-generated mypy test_code
-
-echo "Running mypy with handwritten stubs"
-MYPYPATH=./stubs-handwritten mypy test_code
