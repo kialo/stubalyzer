9ff737cea13a5b209b3544e63f82b103c1f3264a
David Mittelstädt <dm@kialo.com>
2019-11-05 16:47:03 +0100

    Remove unused utils.

--- stubalyzer/utils.py
+++ stubalyzer/utils.py
@@ -1,8 +1,6 @@
-from typing import List, Optional
+from typing import Optional
 
-from mypy.nodes import ARG_STAR, ARG_STAR2, Expression
-
-from .types import STRICT_ARG_KINDS
+from mypy.nodes import Expression
 
 
 def get_expression_fullname(expr: Expression) -> Optional[str]:
@@ -12,15 +10,3 @@ def get_expression_fullname(expr: Expression) -> Optional[str]:
         fullname_attr = fullname_attr()
 
     return fullname_attr if isinstance(fullname_attr, str) else None
-
-
-def strict_kind_count(kinds: List[int]) -> int:
-    return len([kind for kind in kinds if kind in STRICT_ARG_KINDS])
-
-
-def arg_star_count(kinds: List[int]) -> int:
-    return len([kind for kind in kinds if kind is ARG_STAR])
-
-
-def arg_star2_count(kinds: List[int]) -> int:
-    return len([kind for kind in kinds if kind is ARG_STAR2])
--- stubalyzer/utils_unit_test.py
+++ stubalyzer/utils_unit_test.py
@@ -1,21 +1,8 @@
 from unittest.mock import Mock
 
-from mypy.nodes import (
-    ARG_NAMED,
-    ARG_NAMED_OPT,
-    ARG_OPT,
-    ARG_POS,
-    ARG_STAR,
-    ARG_STAR2,
-    Expression,
-)
+from mypy.nodes import Expression
 
-from .utils import (
-    arg_star2_count,
-    arg_star_count,
-    get_expression_fullname,
-    strict_kind_count,
-)
+from .utils import get_expression_fullname
 
 
 class TestUtils:
@@ -48,21 +35,3 @@ class TestUtils:
         fullname_mock = Mock(return_value=13245)
         expression = Mock(fullname=fullname_mock, spec=Expression)
         assert get_expression_fullname(expression) is None
-
-    def test_strict_kind_count(self) -> None:
-        kinds = [ARG_POS, ARG_OPT, ARG_STAR, ARG_NAMED, ARG_STAR2, ARG_NAMED_OPT]
-        assert strict_kind_count(kinds) == 4
-        assert strict_kind_count([]) == 0
-        assert strict_kind_count([ARG_STAR, ARG_STAR2]) == 0
-
-    def test_arg_star_count(self) -> None:
-        kinds = [ARG_POS, ARG_OPT, ARG_STAR, ARG_NAMED, ARG_STAR2, ARG_NAMED_OPT]
-        assert arg_star_count(kinds) == 1
-        assert arg_star_count([]) == 0
-        assert arg_star_count([ARG_STAR, ARG_STAR]) == 2
-
-    def test_arg_star2_count(self) -> None:
-        kinds = [ARG_POS, ARG_OPT, ARG_STAR, ARG_NAMED, ARG_STAR2, ARG_NAMED_OPT]
-        assert arg_star2_count(kinds) == 1
-        assert arg_star2_count([]) == 0
-        assert arg_star2_count([ARG_STAR2, ARG_STAR2]) == 2
