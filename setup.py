46a476a9195b4f74f3534fe77f93521480b01ae3
Simon Kalt <ska@kialo.com>
2019-12-05 11:15:24 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 # Always prefer setuptools over distutils
 from setuptools import setup
 
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 # This is the name of the GitHub repo and the package name on pypi
 PACKAGE_NAME = "stubalyzer"
 
