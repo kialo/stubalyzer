4759a25f280aaab546980ab441a597f5d0af0a89
Simon Kalt <ska@kialo.com>
2022-01-04 12:49:48 +0100

    Adapt release script to auto tag, push and publish

--- dev/release.sh
+++ dev/release.sh
@@ -4,8 +4,10 @@ set -e
 source "$(dirname $0)/ensure_venv.sh"
 source "$(dirname $0)/util.sh"
 
-function get_version {
-    python -c 'print(__import__("stubalyzer").__version__)'
+old_version="v$(get_version)"
+
+function changes {
+    git log --abbrev-commit --format='-  %s - %h' ${old_version}..HEAD | cat
 }
 
 if [ -z "$1" ]; then
@@ -16,14 +18,46 @@ fi
 echo "Cleaning up build and dist output..."
 rm -rf build dist
 
-old_version="v$(get_version)"
-
 echo "Bumping $1 version..."
 bump2version $@
 
 new_version="v$(get_version)"
 
+echo ""
 echo "Bumped version from $old_version to $new_version"
+
+echo ""
+if confirm "Tag new version" "Y"; then
+    echo "Tagging ${new_version}..."
+    cat << EOF | git tag -s ${new_version} -F-
+Release ${new_version}
+
+$(changes)
+EOF
+    tagged=true
+else
+    tagged=false
+    echo "Not tagging ${new_version}."
+fi
+
+echo ""
+if $tagged && confirm "Push tag ${new_version} to remote?"; then
+    echo "Pushing tag ${new_version}..."
+    git push origin ${new_version}
+else
+    echo "Not pushing tag."
+fi
+
+echo ""
+if confirm "Publish ${new_version} to pypi?"; then
+    echo "Publishing ${new_version} to pypi..."
+    flit publish
+    echo "git push origin ${new_version}"
+else
+    echo "Not publishing to pypi."
+fi
+
+
 echo ""
 tput smso
 echo "Please update the 'Commits' link under 'Development' in CHANGELOG.rst to the following:"
@@ -49,23 +83,12 @@ echo "$heading_line"
 echo ""
 echo "\`Commits <https://github.com/kialo/stubalyzer/compare/${old_version}...${new_version}>\`__"
 echo ""
-git log --abbrev-commit --format='-  %s - %h' ${old_version}..HEAD | cat
+changes
 echo ""
 tput smso
 echo ">>>>>>>>>>>>>>>>"
 tput rmso
 
 tput smso
-echo "Once updated, commit the changes, tag, push and upload to PyPI:"
-echo "<<<<<<<<<<<<<<<<"
-tput rmso
-echo ""
-echo "git commit -a -m 'Update changelog'"
-echo "git tag -s ${new_version}"
-echo "git push"
-echo "git push --tags"
-echo "flit publish"
-echo ""
-tput smso
-echo ">>>>>>>>>>>>>>>>"
+echo "Once updated, commit the changes and push."
 tput rmso
--- dev/util.sh
+++ dev/util.sh
@@ -32,3 +32,43 @@ function length {
 function dashes {
     printf -- '-%.s' $(eval "echo {1.."$(($1))"}")
 }
+
+function get_version {
+    python -c 'print(__import__("stubalyzer").__version__)'
+}
+
+function confirm() {
+    local prompt default reply
+
+    if [[ ${2:-} = 'Y' ]]; then
+        prompt='Y/n'
+        default='Y'
+    elif [[ ${2:-} = 'N' ]]; then
+        prompt='y/N'
+        default='N'
+    else
+        prompt='y/n'
+        default=''
+    fi
+
+    while true; do
+
+        # Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo -n "$1 [$prompt] "
+
+        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        read -r reply </dev/tty
+
+        # Default?
+        if [[ -z $reply ]]; then
+            reply=$default
+        fi
+
+        # Check if the reply is valid
+        case "$reply" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+
+    done
+}
